3870c6b399d2860d95f4a86511b8cd45aa67b6ef
Lefie <qiwenruan11@gmail.com>
2025-04-28 15:21:46 -0400

    project setup

---
+++ index.jsx
@@ -0,0 +1,12 @@
+
+import { createRoot } from 'react-dom/client'
+
+function App() {
+  return (
+    <h1>hello world </h1>
+  )
+}
+
+createRoot(document.getElementById('root')).render(
+    <App />
+)
