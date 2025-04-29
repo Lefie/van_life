324343b60a44cbb31daa5337e5919c106024fb00
Lefie <qiwenruan11@gmail.com>
2025-04-28 21:55:10 -0400

    fix link

--- index.jsx
+++ index.jsx
@@ -4,12 +4,16 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import Nav from './ui_components/Nav'
 import Main from './ui_components/Main'
 import Footer from './ui_components/Footer'
+import Vans from './ui_components/Vans'
+import About from './ui_components/About'
 
 function App() {
   return (
   <BrowserRouter>
     <Routes>  
         <Route path="/" element={<Home />}  />
+        <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans />} />
     </Routes>
   </BrowserRouter>
   )
--- ui_components/Nav.jsx
+++ ui_components/Nav.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import logo from "../assets/logo.png"
+import { Link } from "react-router-dom";
+
 
 export default function Nav() {
     
-    console.log(logo, "from Nav.jsx")
+
 
     return (
         <>
         <div className="nav-bar">
             <img className="logo-img" src={logo} />
             <div className="menu_list">
-                <p>About</p>
-                <p>Vans</p>
+                <Link to="/about"><p>About</p></Link>
+                <Link to="/vans"><p>Vans</p></Link>
             </div>
         </div>
         </>
